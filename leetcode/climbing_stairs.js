6c3519472d91ae717c5c778c7a22d8fe8592529e
imhotep <r.sipchenko@gmail.com>
2022-12-30 14:58:19 -0700

    add climbing stairs to leetcode

---
+++ leetcode/climbing_stairs.js
@@ -0,0 +1,14 @@
+var climbStairs = function (n) {
+  if (n == 1) return 1;
+
+  let first = 1;
+  let second = 2;
+
+  for (let i = 3; i <= n; i++) {
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return second;
+};
